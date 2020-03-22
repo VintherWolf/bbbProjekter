e94cf353c66469911dd6d6e976771ca2716c90b4
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-03-22 13:25:38 +0100

    Interchange Input and Output in SW

--- bbb_task3_nopru/no_pru.cpp
+++ bbb_task3_nopru/no_pru.cpp
@@ -1,35 +1,35 @@
 ////////////////////////////////////////////////////////////////////////////////
-// 	Source File	:	no_pru_main.c
-//	Author			: Daniel Wolf
-//  Date				: 22 Mar 2020
-//	Version			: 1
+// 	Source File	: no_pru.c
+//	Author		: Daniel Wolf
+//  Date		: 22 Mar 2020
+//	Version		: 1
 //
 //	Description	: Toggler pin P8-45 som aftastet af P9-31
-//	DoxyUsage		: @ brief
-//								"@tparam for template arguments"
-//								"@arg for function arguments"
-//								"@return for return values"
-//								"@brief for intro/description"
-//								"@details for highlighting details"
+//	DoxyUsage	: @ brief
+//				"@tparam for template arguments"
+//				"@arg for function arguments"
+//				"@return for return values"
+//				"@brief for intro/description"
+//				"@details for highlighting details"
 ///////////////////////////////////////80///////////////////////////////////////
 
-#include<iostream>
-#include"GPIO.h"
+#include <iostream>
+#include "GPIO.h"
 
 using namespace exploringBB;
 using namespace std;
 
-// Output: P8_45 GPIO2_6 = 2x32 + 6 = 70
-// Input: P9_31 GPIO3_14 = 3x32 + 14 = 110
+// Input: P8_45 GPIO2_6 = 2x32 + 6 = 70
+// Output: P9_31 GPIO3_14 = 3x32 + 14 = 110
 
-int main ( )
+int main()
 {
-	GPIO outGPIO (70), inGPIO (110);
-	outGPIO.setDirection (OUTPUT);
-	inGPIO.setDirection (INPUT);
+	GPIO outGPIO(110), inGPIO(70);
+	outGPIO.setDirection(OUTPUT);
+	inGPIO.setDirection(INPUT);
 
 	// Enable output
-	outGPIO.streamOpen ();
+	outGPIO.streamOpen();
 
 	for (;;)
 	{
@@ -37,11 +37,11 @@ int main ( )
 		{
 			outGPIO.streamWrite(LOW);
 		}
-		outGPIO.streamWrite (HIGH);
+		outGPIO.streamWrite(HIGH);
 	}
 
 	// Disable output
-	outGPIO.streamClose ();
+	outGPIO.streamClose();
 
 	return 0;
 }
